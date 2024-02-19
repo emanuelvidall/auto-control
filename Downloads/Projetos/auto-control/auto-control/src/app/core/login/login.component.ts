ae5783d5f801f21ce7f1f6a6d59c2cf7c01a05fd
Emanuel Vidal <emanuelvidal@unifor.br>
2024-02-19 11:41:49 -0300

    minor adjustments

--- Downloads/Projetos/auto-control/auto-control/src/app/core/login/login.component.ts
+++ Downloads/Projetos/auto-control/auto-control/src/app/core/login/login.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
-import { SubmitButtonComponent } from '../submit-button/submit-button.component';
+import { Component } from '@angular/core'
+import { SubmitButtonComponent } from '../submit-button/submit-button.component'
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [SubmitButtonComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  carImagePath: string = 'assets/car.jpg';
-  logoPath: string = 'assets/logo.png';
+  carImagePath: string = 'assets/car.jpg'
+  logoPath: string = 'assets/logo.png'
   AlternateLogoPath: string = 'assets/logo-alternate.png'
+  eyeOn: string = 'assets/eye.svg'
+  eyeOff: string = 'assets/eye-off.svg'
 }
