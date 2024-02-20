e5f70184b1e34e7324fcfc32ecde6b80d308af0a
Emanuel Vidal <emanuelvidal@unifor.br>
2024-02-20 17:30:51 -0300

    http requests added

--- src/app/core/home/home.module.ts
+++ src/app/core/home/home.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { HomeRoutingModule } from './home-routing.module';
-
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http'
+import { HomeRoutingModule } from './home-routing.module'
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule, HttpClientModule],
+  providers: [provideHttpClient(withFetch())],
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,17 +1,51 @@
 import { Component } from '@angular/core'
 import { SubmitButtonComponent } from '../submit-button/submit-button.component'
+import { CommonModule } from '@angular/common'
+import { HttpClient } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [SubmitButtonComponent],
+  imports: [SubmitButtonComponent, CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private http: HttpClient) {}
+
   carImagePath: string = 'assets/car.jpg'
   logoPath: string = 'assets/logo.png'
   AlternateLogoPath: string = 'assets/logo-alternate.png'
   eyeOn: string = 'assets/eye.svg'
   eyeOff: string = 'assets/eye-off.svg'
+
+  showEye: boolean = false
+  showPassword: boolean = false
+
+  userData = {
+    email: '',
+    password: '',
+  }
+
+  toggleEye() {
+    this.showEye = !this.showEye
+    this.showPassword = !this.showPassword
+  }
+
+  clearForm() {
+    this.userData.email = ''
+    this.userData.password = ''
+  }
+
+  submitLogin() {
+    const url = 'https://httpbin.org/post'
+    this.http.post(url, this.userData).subscribe({
+      next: (response) => {
+        this.clearForm()
+        console.log(response)
+      },
+      error: (error) => console.error(error),
+    })
+  }
 }
