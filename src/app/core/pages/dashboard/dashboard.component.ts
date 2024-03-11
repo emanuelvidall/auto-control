b5d12e8a6f9aee33eeb06ff2d9ab579e4d4103ee
Emanuel Vidal <emanuelvidalrm@gmail.com>
2024-03-11 00:24:03 -0300

    :sparkles: car map

--- src/app/core/pages/dashboard/dashboard.component.ts
+++ src/app/core/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
 import { CarComponentComponent } from '../../components/car-component/car-component.component'
 import { Car } from '../../components/car-component/car-interface'
+import { NgFor } from '@angular/common'
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CarComponentComponent],
+  imports: [CarComponentComponent, NgFor],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
@@ -13,8 +14,8 @@ export class DashboardComponent {
   AlternateLogoPath: string = 'assets/logo-alternate.png'
 
   cars: Car[] = [
-    { id: 1, make: 'Toyota', model: 'Corolla', year: 2020 },
-    { id: 2, make: 'Honda', model: 'Civic', year: 2019 },
-    { id: 3, make: 'Ford', model: 'Mustang', year: 2021 },
+    { id: 1, make: 'Toyota', model: 'Corolla', year: 2020, type: 'car' },
+    { id: 2, make: 'Honda', model: 'Civic', year: 2019, type: 'car' },
+    { id: 3, make: 'Ford', model: 'Mustang', year: 2021, type: 'car' },
   ]
 }
