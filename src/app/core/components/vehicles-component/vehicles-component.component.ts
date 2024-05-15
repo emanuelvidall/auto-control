19e41a0ea6a5db1c5d940870f871bf063b91a626
Rolf Matela <rolf.matela@unifor.br>
2024-05-15 17:05:21 -0300

    alterações na tela de Vehicles

--- src/app/core/components/vehicles-component/vehicles-component.component.ts
+++ src/app/core/components/vehicles-component/vehicles-component.component.ts
@@ -1,37 +1,54 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
-import { DataService, Vehicle } from '../../services/data.service';
+import { Component, Input, OnInit } from '@angular/core'
+import { animate, state, style, transition, trigger } from '@angular/animations'
+import { CommonModule } from '@angular/common'
+import { MatTableModule } from '@angular/material/table'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { DataService, Vehicle } from '../../services/data.service'
 
 @Component({
   selector: 'app-vehicles-component',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule],
   templateUrl: './vehicles-component.component.html',
-  styleUrls: ['./vehicles-component.component.scss']
+  styleUrls: ['./vehicles-component.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class VehiclesComponentComponent implements OnInit {
-  @Input() vehicles: Vehicle[] = [];  // Initialize with an empty array
-  userName: string = '';
+  @Input() vehicles: Vehicle[] = [] // Initialize with an empty array
+  userName: string = ''
 
   constructor(private dataService: DataService) {}
 
-  displayedColumns: string[] = ['name', 'type_name', 'brand_name', 'owner_name'];
-  dataSource: Vehicle[] = [];  // Initialize with the correct type
+  displayedColumns: string[] = ['name', 'type_name', 'brand_name', 'owner_name']
+  displayedColumnsWithExpand = [...this.displayedColumns, 'expand']
+  dataSource: Vehicle[] = [] // Initialize with the correct type
+  expandedVehicle: Vehicle | null = null
 
   ngOnInit() {
-    const userData = this.dataService.getUserData();
+    const userData = this.dataService.getUserData()
     if (userData) {
-      this.userName = userData.user_name;
-      this.dataService.getVehiclesById(userData.user_id, userData.token).subscribe(
-        (vehicles: Vehicle[]) => {
-          this.vehicles = vehicles;
-          this.dataSource = vehicles;
-        },
-        (error) => {
-          console.error('Error fetching vehicles: ', error);
-        }
-      );
+      this.userName = userData.user_name
+      this.dataService
+        .getVehiclesById(userData.user_id, userData.token)
+        .subscribe(
+          (vehicles: Vehicle[]) => {
+            this.vehicles = vehicles
+            this.dataSource = vehicles
+          },
+          (error) => {
+            console.error('Error fetching vehicles: ', error)
+          }
+        )
     }
   }
 }
--- src/app/core/pages/vehicles/vehicles.component.ts
+++ src/app/core/pages/vehicles/vehicles.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehiclesComponentComponent } from '../../components/vehicles-component/vehicles-component.component';
-import { DataService, Vehicle } from '../../services/data.service';
+import { NavbarComponent } from '../../components/navbar/navbar.component'
 
 @Component({
   selector: 'app-vehicles',
   standalone: true,
-  imports: [CommonModule, VehiclesComponentComponent],
+  imports: [CommonModule, VehiclesComponentComponent, NavbarComponent],
   templateUrl: './vehicles.component.html',
   styleUrl: './vehicles.component.scss'
 })
