08b5788f90f84def5566d9da8bf9c370ae314ce7
Rolf Matela <rolf.matela@unifor.br>
2024-05-20 16:35:01 -0300

    :recycle: refactor: modified some methods
    altered some methods and the way that they were called

--- src/app/core/components/vehicles-component/vehicles-component.component.ts
+++ src/app/core/components/vehicles-component/vehicles-component.component.ts
@@ -1,17 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { animate, state, style, transition, trigger } from '@angular/animations'
-import { CommonModule } from '@angular/common'
+import { CommonModule, NgFor, NgIf } from '@angular/common'
 import { MatTableModule } from '@angular/material/table'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
-import { DataService, Vehicle } from '../../services/data.service'
 import { MatDialog } from '@angular/material/dialog'
+import { DataService, Vehicle } from '../../services/data.service'
 import { ExpenseDialogComponent } from '../expense-dialog/expense-dialog.component'
 
 @Component({
   selector: 'app-vehicles-component',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    NgFor,
+    NgIf,
+  ],
   templateUrl: './vehicles-component.component.html',
   styleUrls: ['./vehicles-component.component.scss'],
   animations: [
@@ -26,42 +33,54 @@ import { ExpenseDialogComponent } from '../expense-dialog/expense-dialog.compone
   ],
 })
 export class VehiclesComponentComponent implements OnInit {
-  @Input() vehicles: Vehicle[] = [] // Initialize with an empty array
+  @Input() vehicles: Vehicle[] = []
+
   userName: string = ''
+  displayedColumns: string[] = ['name', 'type_name', 'brand_name', 'owner_name']
+  displayedColumnsWithExpand = [...this.displayedColumns, 'expand']
+  dataSource: Vehicle[] = []
+  expandedVehicle: Vehicle | null = null
+
+  columnHeaders: {[key: string]: string} = {
+    name: 'Nome',
+    type_name: 'Tipo',
+    brand_name: 'Marca',
+    owner_name: 'Proprietário',
+    expand: 'Expandir'
+  }
 
   constructor(private dataService: DataService, private dialog: MatDialog) {}
 
-  displayedColumns: string[] = ['name', 'type_name', 'brand_name', 'owner_name']
-  displayedColumnsWithExpand = [...this.displayedColumns, 'expand']
-  dataSource: Vehicle[] = [] // Initialize with the correct type
-  expandedVehicle: Vehicle | null = null
-
   ngOnInit() {
+    this.loadUserData()
+  }
+
+  private loadUserData(): void {
     const userData = this.dataService.getUserData()
     if (userData) {
       this.userName = userData.user_name
-      this.dataService
-        .getVehiclesById(userData.user_id, userData.token)
-        .subscribe(
-          (vehicles: Vehicle[]) => {
-            this.vehicles = vehicles
-            this.dataSource = vehicles
-          },
-          (error) => {
-            console.error('Error fetching vehicles: ', error)
-          }
-        )
+      this.loadVehicles(userData.user_id, userData.token)
     }
   }
 
+  private loadVehicles(userId: number, token: string): void {
+    this.dataService.getVehiclesById(userId, token).subscribe(
+      (vehicles: Vehicle[]) => {
+        this.vehicles = vehicles
+        this.dataSource = vehicles
+      },
+      (error) => {
+        console.error('Erro ao tentar encontrar veículos: ', error)
+      }
+    )
+  }
+
   openExpenseDialog(vehicleId: number): void {
-    const testId = vehicleId
-    console.log(testId)
     const dialogRef = this.dialog.open(ExpenseDialogComponent, {
       width: '500px',
       data: {
-        testId: testId
-      }
+        vehicleId: vehicleId,
+      },
     })
 
     dialogRef.afterClosed().subscribe((result) => {
