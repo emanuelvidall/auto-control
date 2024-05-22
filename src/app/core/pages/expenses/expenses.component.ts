6a5f2f9ed8a65f1f8532eb7dcab7e9b927d7fabd
Emanuel Vidal <emanuelvidal@unifor.br>
2024-05-22 10:51:27 -0300

    expenses page

--- src/app/core/pages/expenses/expenses.component.ts
+++ src/app/core/pages/expenses/expenses.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core'
 import { ExpenseSidebarComponent } from '../../components/expense-sidebar/expense-sidebar.component'
 import { NavbarComponent } from '../../components/navbar/navbar.component'
+import { ExpenseTableComponent } from '../../components/expense-table/expense-table.component'
+import { ExpenseChartComponent } from '../../components/expense-chart/expense-chart.component'
 
 @Component({
   selector: 'app-expenses',
   standalone: true,
-  imports: [ExpenseSidebarComponent, NavbarComponent],
+  imports: [
+    ExpenseSidebarComponent,
+    NavbarComponent,
+    ExpenseTableComponent,
+    ExpenseChartComponent,
+  ],
   templateUrl: './expenses.component.html',
   styleUrl: './expenses.component.scss',
 })
-export class ExpensesComponent {}
+export class ExpensesComponent {
+  openModal() {
+    // Open modal
+  }
+}
